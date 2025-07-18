be36a6956531489b1943246dfda0ee4da8c1433b
tu11aa <thanhtuhr01@gmail.com>
2025-07-18 16:16:51 +0700

    updat eproject name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "Arb-Stylus",
-  projectName: "scaffold-stylus",
+  projectName: "scaffold-stylus-docs",
   trailingSlash: false,
 
   plugins: [
